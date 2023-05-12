f2a35a9181118f094042efc4e10a1315c951ebf5
illorca <ignacio.rodriguez@access1.hpc.dhclab.i.hpi.de>
2023-05-12 22:26:19 +0200

    Remake mugit.py

--- dicts/mugit.py
+++ dicts/mugit.py
@@ -8,61 +8,41 @@ log = logging.getLogger(__name__)
 
 
 def get_concept_details(cfg) -> dict:
-    umls_meta_path = cfg.dict.custom.umls_meta_path
-    mugit_path = cfg.dict.custom.mugit_path
-    mugit_file = cfg.dict.custom.mugit_file
+    # load snomed terms
+    mugit_snomed_path = cfg.dict.custom.mugit_path
+    mugit = pd.read_csv(mugit_snomed_path, sep="\t", header=None, encoding="utf8")
+    mugit.columns = ["snomed_id", "term_id", "en", "de"]
+    mugit.sort_index(inplace=True)
+    mugit.dropna(inplace=True)
+
+    # get lang from cfg. If no lang specified, use both
+    lang = cfg.dict.custom.lang if "lang" in cfg.dict.custom else ['de', 'en']
 
     concept_details = {}
+    # both languages selected, english ones go to canonical and all different terms to alias
+    if lang == ['de', 'en']:
+        for _, entry in mugit.iterrows():
+            sid = entry.snomed_id
+            if not sid in concept_details:
+                concept_details[sid] = {"concept_id": sid, "canonical_name": entry.en, "types": [], "aliases": [entry.de]}
+            elif sid in concept_details:
+                if entry.en not in concept_details[sid]["aliases"] and entry.en != concept_details[sid]["canonical_name"]:
+                    concept_details[sid]["aliases"].append(entry.en)
+                if entry.de not in concept_details[sid]["aliases"]:
+                    concept_details[sid]["aliases"].append(entry.de)
 
-    log.info(">> Reading concepts ...")
+    # just one language
+    elif lang == ["en"] or lang == ["de"]:
+        l = lang[0]
+        for _, entry in mugit.iterrows():
+            sid = entry.snomed_id
+            if not sid in concept_details:
+                concept_details[sid] = {"concept_id": sid, "canonical_name": entry[l], "types": [], "aliases": []}
+            elif sid in concept_details:
+                if entry[l] not in concept_details[sid]["aliases"] and entry[l] != concept_details[sid]["canonical_name"]:
+                    concept_details[sid]["aliases"].append(entry[l])
 
-    mugit_interface = pd.read_csv(Path(mugit_path) / mugit_file, sep="\t", header=None)
-    mugit_interface.columns = ["SNOMED_ID", "TERM_ID", "English", "German"]
-    mugit_interface.set_index("SNOMED_ID", inplace=True)
-    mugit_interface.sort_index(inplace=True)
-    mugit_interface.dropna(inplace=True)
-
-    concepts_filename = "MRCONSO.RRF"
-    headers = umls_utils.read_umls_file_headers(umls_meta_path, concepts_filename)
-
-    snomed_ids = set()
-
-    with open(f"{umls_meta_path}/{concepts_filename}") as fin:
-        for line in fin:
-            splits = line.strip().split("|")
-            assert len(headers) == len(splits), (headers, splits)
-            concept = dict(zip(headers, splits))
-            if concept["SAB"] == "SNOMEDCT_US":
-                snomed_id = int(concept["CODE"])
-                if snomed_id in snomed_ids:
-                    continue
-                snomed_ids.add(snomed_id)
-                if snomed_id in mugit_interface.index:
-                    interface_terms = mugit_interface.loc[[snomed_id]]
-                    concept_id = concept["CUI"]
-                    if concept_id not in concept_details:
-                        concept_details[concept_id] = {
-                            "concept_id": concept_id,
-                            "canonical_name": None,
-                            "types": [],
-                            "aliases": interface_terms.German.tolist(),
-                        }
-                    else:
-                        concept_details[concept_id]["aliases"] += interface_terms.German.tolist()
-
-    # Remove duplicates
-    for v in concept_details.values():
-        v["aliases"] = list(set(v["aliases"]))
-
-    log.info(">> Reading types ... ")
-    umls_utils.read_umls_types(umls_meta_path, concept_details)
-
-    log.info(">> Reading definitions ... ")
-    umls_utils.read_umls_definitions(umls_meta_path, concept_details)
-
-    for concept in concept_details.values():
-        # deleting `is_from_preferred_source`
-        if "is_from_preferred_source" in concept:
-            del concept["is_from_preferred_source"]
+    else:
+        print("Languages not supported by MUGIT")
 
     return concept_details
