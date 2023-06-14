11d14fa5bed9dcaf00ff49be1bdedba0179d9dda
illorca <ignacio.rodriguez@access2.hpc.dhclab.i.hpi.de>
2023-06-14 10:39:03 +0200

    More black

--- dicts/ops2017.py
+++ dicts/ops2017.py
@@ -17,19 +17,11 @@ def get_concept_details(cfg) -> dict:
 
     # load systematic codes
     syst_cols = list(range(30))
-    (
-        syst_cols[3],
-        syst_cols[6],
-        syst_cols[8],
-        syst_cols[4],
-        syst_cols[5],
-    ) = (
-        "type",
-        "code",
-        "text",
-        "code2",
-        "code3",
-    )
+    syst_cols[3] = "type"
+    syst_cols[6] = "code"
+    syst_cols[8] = "text"
+    syst_cols[4] = "code2"
+    syst_cols[5] = "code3"
     syst = pd.read_csv(f"{path}/p1smt2017/Klassifikationsdateien/ops2017syst_kodes.txt", sep=";", names=syst_cols)
 
     # get the codes that are only in the systematic list
