21dbb13978b76bc93ccdf2c8f05e34c367f93cdd
illorca <ignacio.rodriguez@access2.hpc.dhclab.i.hpi.de>
2023-06-14 12:00:31 +0200

    Get all codes from ATC and change Notebook to medications

--- dicts/atc2017_de.py
+++ dicts/atc2017_de.py
@@ -7,21 +7,23 @@ import openpyxl
 def get_concept_details(cfg) -> dict:
     path = cfg.dict.custom.atc_path
     wb = openpyxl.load_workbook(f"{path}/ATC GKV AI 2017.xlsx")
-    sheet = wb["WIdO-Index alphabetisch_2017"]
-    atc_dict = pd.DataFrame(sheet.values)
-    atc_dict = atc_dict.rename(columns={0: "code", 2: "text", 4: "DDD_Info"})
-    atc_dict.drop(0, axis=0, inplace=True)
-    atc_dict.dropna(axis=0, how="all", inplace=True)
-    atc_dict.drop(atc_dict[atc_dict.text.isnull()].index, inplace=True)
-    atc_dict.drop(atc_dict[atc_dict.code.isnull()].index, inplace=True)
+    sheet = wb["WIdO-Index sortiert_2017"]
+    sort = pd.DataFrame(sheet.values)
+    sort = sort.rename(columns={0: "code", 2: "text", 4: "DDD_Info"})
+    sort.drop(0, axis=0, inplace=True)
+    sort.dropna(axis=0, how="all", inplace=True)
+    sort.drop(sort[sort.text.isnull()].index, inplace=True)
+    sort.drop(sort[sort.code.isnull()].index, inplace=True)
+
+    sort["code"] = sort["code"].str.rstrip()
+    sort = sort[sort["code"].str.len() > 3]
 
     concept_details = {}
-    for _, entry in atc_dict.iterrows():
+    for _, entry in sort.iterrows():
         sid = entry.code
-        if sid is not None:
-            if not sid in concept_details:
-                concept_details[sid] = {"concept_id": sid, "canonical_name": entry.text, "types": [], "aliases": []}
-            elif sid in concept_details:
-                concept_details[sid]["aliases"].append(entry.text)
+        if not sid in concept_details:
+            concept_details[sid] = {"concept_id": sid, "canonical_name": entry.text, "types": [], "aliases": []}
+        elif sid in concept_details:
+            concept_details[sid]["aliases"].append(entry.text)
 
     return concept_details
