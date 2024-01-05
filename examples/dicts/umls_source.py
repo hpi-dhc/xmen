04a414bd2ac6094991d579ef965e077df73fb78e
Florian Borchert <Florian.Borchert@hpi.de>
2024-01-05 14:30:11 +0100

    Black

--- examples/dicts/umls_source.py
+++ examples/dicts/umls_source.py
@@ -3,6 +3,7 @@ from xmen.umls import read_umls_file_headers, get_umls_concepts
 from collections import defaultdict
 from xmen.log import logger
 
+
 def get_concept_details(cfg):
     mrconso = "MRCONSO.RRF"
     concept_details = {}
@@ -23,12 +24,14 @@ def get_concept_details(cfg):
                 cui = concept["CUI"]
                 if id_key:
                     sid = concept[id_key]
-                else:                    
+                else:
                     sid = concept["SDUI"]
                     if not sid:
                         sid = concept["SCUI"]
                 if not sid:
-                    logger.warn(f"Skipping concept with CUI {cui} because we could not find a valid source vocabulary ID")
+                    logger.warn(
+                        f"Skipping concept with CUI {cui} because we could not find a valid source vocabulary ID"
+                    )
                 name = concept["STR"]
                 if sid in concept_details:
                     concept_details[sid]["aliases"].append(name)
@@ -36,19 +39,25 @@ def get_concept_details(cfg):
                     concept_details[sid] = {"concept_id": sid, "canonical_name": name, "types": [], "aliases": []}
                 scui2cui[sid].append(cui)
 
-    if umls_extend := cfg.dict.custom.get('umls_extend'):
+    if umls_extend := cfg.dict.custom.get("umls_extend"):
         # Optionally extend with UMLS synonyms
-        other_umls_concepts = get_umls_concepts(meta_path, 
-            umls_extend.get("lang"), sabs=umls_extend.get("sabs"), sources=umls_extend.get("sources"), semantic_groups=umls_extend.get("semantic_groups"), semantic_types=umls_extend.get("semantic_types"))
+        other_umls_concepts = get_umls_concepts(
+            meta_path,
+            umls_extend.get("lang"),
+            sabs=umls_extend.get("sabs"),
+            sources=umls_extend.get("sources"),
+            semantic_groups=umls_extend.get("semantic_groups"),
+            semantic_types=umls_extend.get("semantic_types"),
+        )
 
         for scui, concept in tqdm(concept_details.items()):
             for cui in scui2cui[scui]:
                 if cui in other_umls_concepts:
-                    for t in other_umls_concepts[cui]['types']:
+                    for t in other_umls_concepts[cui]["types"]:
                         if not t in concept["types"]:
                             concept["types"].append(t)
                     for new_alias in other_umls_concepts[cui]["aliases"] + [other_umls_concepts[cui]["canonical_name"]]:
                         if new_alias not in concept["aliases"] and new_alias != concept["canonical_name"]:
-                            concept["aliases"].append(new_alias)                    
-    
+                            concept["aliases"].append(new_alias)
+
     return concept_details
--- examples/util.py
+++ examples/util.py
@@ -1,15 +1,25 @@
 import pandas as pd
 
+
 def get_dataframe(predictions, kb):
     ents = []
     for d in predictions:
-        for e in d['entities']:
-            span = ' '.join(e['text'])
-            label = e['type']
-            top_concept = e['normalized'][0] if len(e['normalized']) > 0 else None        
+        for e in d["entities"]:
+            span = " ".join(e["text"])
+            label = e["type"]
+            top_concept = e["normalized"][0] if len(e["normalized"]) > 0 else None
             if top_concept:
-                cui = top_concept['db_id']
-                ents.append({'mention' : span, 'class' :  label, 'cui' : cui, 'canonical name' : kb.cui_to_entity[cui].canonical_name, 'linked by' : top_concept['predicted_by'], 'score' : top_concept['score']})
+                cui = top_concept["db_id"]
+                ents.append(
+                    {
+                        "mention": span,
+                        "class": label,
+                        "cui": cui,
+                        "canonical name": kb.cui_to_entity[cui].canonical_name,
+                        "linked by": top_concept["predicted_by"],
+                        "score": top_concept["score"],
+                    }
+                )
             else:
-                ents.append({'mention' : span, 'class' :  label, 'cui' : 'Not linkable'})
+                ents.append({"mention": span, "class": label, "cui": "Not linkable"})
     return pd.DataFrame(ents)
